b13801d4d3c0044d26ec18a170cfd59bf6573df3
Julian Gründner <juliangruendner@googlemail.com>
2017-11-23 09:54:27 +0100

    add jupyter resource and starting jupyter to dockerApi

--- src/dockerAPI.py
+++ src/dockerAPI.py
@@ -2,6 +2,9 @@ from flask import Flask
 from flask_restful import Api
 # from rdb.rdb import connect_to_db, create_all
 from resources.testResource import TestResource
+from resources.modelResource import ModelListResource
+from resources.jupyterResource import JupyterResource
+
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -9,7 +12,8 @@ api = Api(app)
 
 
 api.add_resource(TestResource, '/tests', endpoint='tests')
-
+api.add_resource(ModelListResource, '/models', endpoint='models')
+api.add_resource(JupyterResource, '/jupyter', endpoint='jupyter')
 
 if __name__ == '__main__':
     # connect_to_db(app, 'postgresql://mad:MAD@db:5432/mad')
---
+++ src/startJupyter.sh
@@ -0,0 +1,2 @@
+cd /mlenvironment
+su jupyter bash -c 'jupyter notebook --ip=0.0.0.0 --port=8000 &> jupyterToken.txt &'
