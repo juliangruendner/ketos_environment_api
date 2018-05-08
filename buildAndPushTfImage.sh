1b50771c212ae4f2254e7861d798f6a7495acc26
marcel <marcel.erpenbeck@web.de>
2018-05-08 14:04:29 +0200

    added Dockerfile.API.prodTf and edited ubuntu_jupyter_tf/Dockerfile

---
+++ buildAndPushTfImage.sh
@@ -0,0 +1,6 @@
+cd docker/ubuntu_jupyter_tf
+docker build -t gruendner.de:5043/mlservicecontainer_tf .
+cd ..
+docker build -f Dockerfile.API.prodTf -t gruendner.de:5043/mlservicecontainer_prod_tf .
+docker push gruendner.de:5043/mlservicecontainer_tf
+docker push gruendner.de:5043/mlservicecontainer_prod_tf
