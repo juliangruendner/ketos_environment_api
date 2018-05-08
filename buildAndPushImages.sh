855b217c1825b73f2a8ed4d517f7517c851348ae
marcel <marcel.erpenbeck@web.de>
2018-05-08 20:31:41 +0200

    undo commented code

--- buildAndPushImages.sh
+++ buildAndPushImages.sh
@@ -2,5 +2,5 @@ cd docker/ubuntu_jupyter_r
 docker build -t gruendner.de:5043/mlservicecontainer_r .
 cd ..
 docker build -f Dockerfile.API.prod -t gruendner.de:5043/mlservicecontainer_prod_r .
-#docker push gruendner.de:5043/mlservicecontainer_r
-#docker push gruendner.de:5043/mlservicecontainer_prod_r
+docker push gruendner.de:5043/mlservicecontainer_r
+docker push gruendner.de:5043/mlservicecontainer_prod_r
