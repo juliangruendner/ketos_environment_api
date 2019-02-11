fce338f216db8b857f8bf79470a64d19ecaf1295
Julian Gründner <juliangruendner@googlemail.com>
2019-02-11 18:58:30 +0100

    start jupyter with given token

--- src/startJupyter.sh
+++ src/startJupyter.sh
@@ -2,5 +2,5 @@
 cd /mlenvironment
 # su jupyter bash -c 'jupyter notebook --ip=0.0.0.0 --port=8000 --NotebookApp.token=$1 &'
 chown -R jovyan:users /mlenvironment
-/usr/local/bin/start-notebook.sh --NotebookApp.token="test1234" --port=8000 &
+/usr/local/bin/start-notebook.sh --NotebookApp.token="$1" --port=8000 &
 chown -R jovyan:users /home/jovyan/.local/share/jupyter
